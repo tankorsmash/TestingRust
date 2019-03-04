82453a82951c97212ab37284ab0fbd10208bad38
tankorsmash <tankorsmash@gmail.com>
2019-03-03 22:31:55 -0500

    use mutable vectors for printing

--- test.rs
+++ test.rs
@@ -1,20 +1,28 @@
-struct Name {
-    _name : String
-}
-
-impl Name {
-    fn pretty_name (&self) -> String {
-        return "shoot".to_string();
-    }
-}
-
-struct Hero {
-    name: Name
-
-}
+// struct Name {
+//     _name : String
+// }
+//
+// impl Name {
+//     fn pretty_name (&self) -> String {
+//         return "shoot".to_string();
+//     }
+// }
+//
+// struct Hero {
+//     name: Name
+//
+// }
 
 fn main(){
-    //wtf no default constructors or anything. Rust sucks man. Josh Dec 20, 2018
-    let josh = Hero{name: Name{_name:"".to_string()}};
-    println!("Poop {}", josh.name.pretty_name());
+    // //wtf no default constructors or anything. Rust sucks man. Josh Dec 20, 2018
+    // let josh = Hero{name: Name{_name:"".to_string()}};
+
+    let mut my_vector: Vec<i32> = Vec::new();
+    my_vector.push(1);
+    my_vector.push(2);
+    my_vector.push(3);
+
+    for i in my_vector {
+        println!("Poop {}", i);
+    }
 }
