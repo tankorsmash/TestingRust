9faf5021d0569d849e521a1f39fb7843d9ce6537
tankorsmash <tankorsmash@gmail.com>
2020-02-19 14:51:42 -0500

    apply using Rc, RefCell and Weak to Hero/Backpack etc

--- src/main.rs
+++ src/main.rs
@@ -25,39 +25,35 @@ pub trait Behaviour {
 
 // #[derive(Serialize, Deserialize, Debug)]
 #[derive(Debug)]
-struct Actor<'a> {
+struct Actor {
     name: String,
     hp: i32,
     damage: i32,
 
     behaviours: Vec<Box<dyn Behaviour>>,
 
-    backpack: Backpack<'a>
+    backpack: Backpack
 }
 
-struct Backpack<'a> {
+struct Backpack {
     food: String,
     // actor: *mut Actor,
-    actor: Option<&'a Actor<'a>>
+    actor: Weak<RefCell<Actor>>
 
 }
 
-struct BehaveEat<'a> {
+struct BehaveEat {
     // actor: *mut Actor,
-    actor: Option<&'a Actor<'a>>
+    actor: Weak<RefCell<Actor>>
 }
 
-impl Behaviour for BehaveEat<'_> {
+impl Behaviour for BehaveEat {
     fn do_behaviour(&self) -> bool {
-        match self.actor {
-            Some(actor) => {
-                return actor.backpack.food == "orange"
-                // return true
-            },
-            None => { return false }
+        let actor =  self.actor.upgrade();
+        match actor {
+            Some(actor) => { return actor.borrow_mut().backpack.food == "orange"}
+            None => { return false; }
         }
-        // return self.actor.backpack.food == "ss"
-        // return false;
     }
 }
 
@@ -66,13 +62,14 @@ pub trait Battler {
 }
 
 
-impl Battler for &Actor<'_> {
+impl Battler for Actor {
     fn do_battle(&self, other: &mut impl Battler) -> bool {
+        println!("battling!");
         return true;
     }
 }
 
-impl std::fmt::Display for Actor<'_> {
+impl std::fmt::Display for Actor {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Actor: \"{}\", HP: {}, DMG: {}", self.name, self.hp, self.damage)
     }
@@ -90,44 +87,46 @@ impl std::fmt::Debug for dyn Behaviour {
     }
 }
 
-impl std::fmt::Display for Backpack<'_> {
+impl std::fmt::Display for Backpack {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "BEHAVE!")
     }
 }
 
-impl std::fmt::Debug for Backpack<'_> {
+impl std::fmt::Debug for Backpack {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "BEHAVE debug!")
     }
 }
 
 
-fn build_actor<'a>(name: String) -> Actor<'a> {
+fn build_actor(name: String) -> std::rc::Rc<std::cell::RefCell<Actor>> {
     let mut rng = rand::thread_rng();
     let hp =  rng.gen_range(5, 15);
     let damage =  rng.gen_range(1, 5);
 
 
-    let actor = Actor {
+    let actor = Rc::new(RefCell::new(Actor {
         name: name,
         hp: hp,
         damage: damage,
         behaviours: vec!(),
         backpack: Backpack {
             food: "apple".to_string(),
-            actor: None
+            actor: Weak::new()
         },
-    };
+    }));
+    actor.borrow_mut().backpack.actor = Rc::downgrade(&actor);
 
     return actor;
 }
 
 fn serialize() -> Result<String> {
+    println!("begin serialize");
 
     let names = vec!["Josh", "Matt"];
 
-    let mut actores: Vec<Actor> = Vec::new();
+    let mut actores = Vec::new();
 
     for name in names {
         let actor = build_actor(String::from(name));
@@ -137,9 +136,9 @@ fn serialize() -> Result<String> {
     }
 
 
-    let josh : &Actor = &actores[0];
-    let mut matt: &Actor = &actores[1];
-    josh.do_battle(&mut matt);
+    let josh = &actores[0];
+    let mut matt = &actores[1];
+    josh.borrow_mut().do_battle(&mut (*matt.borrow_mut()));
 
     // let data = serde_json::to_string(&josh)?;
     // println!("result:\n{}", data);
@@ -150,13 +149,13 @@ fn serialize() -> Result<String> {
 
 fn behave_testing()
 {
-    // let res = serialize();
+    let res = serialize();
     let actor = build_actor(String::from("Hero"));
     // actor.behaviours.push(Box::new(BehaveEat{actor: Some(&mut actor)}));
-    for behaviour in actor.behaviours {
-        let behave_result = behaviour.do_behaviour();
-        println!("Behaviour {} resulted: '{}'", behaviour, behave_result);
-    }
+    // for behaviour in actor.behaviours {
+    //     let behave_result = behaviour.do_behaviour();
+    //     println!("Behaviour {} resulted: '{}'", behaviour, behave_result);
+    // }
 }
 
 struct Parent
@@ -169,14 +168,25 @@ struct Child
     parent: Weak<RefCell<Parent>>,
 }
 
+pub trait Testing {
+    fn testing(&self) -> ();
+}
+
+impl Testing for Parent {
+    fn testing(&self) -> () {
+        println!("testing");
+    }
+}
 
 fn main(){
     println!("Running...");
+    serialize();
 
-    let mut parent = Rc::new(RefCell::new(Parent {
-        child: Child{ parent: Weak::new() }
-    }));
-    parent.borrow_mut().child.parent = Rc::downgrade(&parent);
+    // let mut parent = Rc::new(RefCell::new(Parent {
+    //     child: Child{ parent: Weak::new() }
+    // }));
+    // parent.borrow_mut().child.parent = Rc::downgrade(&parent);
+    // parent.borrow_mut().testing();
 
     println!("Done!");
 }
